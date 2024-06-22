3d51aad740cf89fd8b8eb25815d0427fbc57aa09
Your Name <you@example.com>
2024-06-22 10:33:32 +0800

    version="1.1",

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="mailtrain-api",
-    version="1.0",
+    version="1.1",
     packages=find_packages(),
     test_suite="tests",
     include_package_data=True,
